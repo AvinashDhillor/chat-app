77ca740b9ae0c0f39af1c73af8f490d568c64478
Avinash Dhillor <avinashdhillor24@gmail.com>
2018-10-26 21:10:37 +0530

    Emit newMessage or

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log("Connected to server")
-
-    socket.emit('createMessage', {
-        from: 'Avinash',
-        text: 'Hey, this is andrew'
-    })
 })
 
 socket.on('disconnect', function() {
