fc652d4f307851e1e0a76952220e906df76abac7
Avinash Dhillor <avinashdhillor24@gmail.com>
2018-10-27 22:08:28 +0530

    App completed using socket.io

---
+++ playground/time.js
@@ -0,0 +1,15 @@
+var moment = require('moment')
+
+// var date = moment();
+// date.add(1, 'years').subtract(2, 'months')
+// console.log(date.format('MMM Do, YYYY'))
+
+new Date().getTime()
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp)
+
+var createdAt = 1234;
+
+var date = moment(createdAt)
+console.log(date.format('hh:mm a'))
